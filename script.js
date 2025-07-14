2c41f8864ebf4bf9b6b5206a4968f23cb081a805
joao <jvbulegon@gmail.com>
2025-07-14 14:01:11 -0300

    Inicio/Template

---
+++ script.js
@@ -0,0 +1,31 @@
+const btnAdicionar = document.getElementById('btnAdicionar');
+const input = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
+let contador = document.getElementById('contador');
+
+let total = 0;
+
+// Funcionalidade 1: adicionar item
+btnAdicionar.addEventListener('click', () => {
+  // Criar elemento <li> com texto do input e botões
+  // Atualizar contador
+});
+
+// Funcionalidade 2: marcar como importante
+function marcarImportante(botao) {
+  // Alternar classe .importante no <li>
+
+}
+
+// Funcionalidade 3: remover item
+function removerItem(botao) {
+  // Remover <li> e atualizar contador
+
+}
+
+// Funcionalidade 4: atualizar contador
+function atualizarContador() {
+  // Atualizar o texto do contador
+
+
+}
